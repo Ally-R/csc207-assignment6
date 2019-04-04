5fcdacbaa539ac919d5d85519e8a17ea7ab83b37
Ally-R <ally.rogers.mail@gmail.com>
2019-04-04 13:54:56 -0500

    Code completed(?) and documentation, started writeup

--- src/assignment6/IntPair.java
+++ src/assignment6/IntPair.java
@@ -1,18 +1,34 @@
 package assignment6;
 
+/**
+ * A simple representation of a pair of integers
+ * 
+ * @author Peter-Michael Osera
+ * @author Samuel A. Rebelsky
+ */
 public class IntPair {
-  private int fst;
-  private int snd;
+  
+  // Fields
+  private int fst; // First int in pair
+  private int snd; // Second int in pair
 
+  // Constructor
   public IntPair(int fst, int snd) {
     this.fst = fst;
     this.snd = snd;
   } // IntPair(int,int)
 
+  // Accessors
+  /**
+   * Get the first int in the pair
+   */
   public int getFst() {
     return fst;
   } // getFst()
 
+  /**
+   * Get the second int in the pair
+   */
   public int getSnd() {
     return snd;
   } // getSnd()
--- src/assignment6/Utils.java
+++ src/assignment6/Utils.java
@@ -1,48 +1,76 @@
 package assignment6;
 
+import java.lang.Math;
+
+/**
+ * Utils for various integer, string and array calculations/manipulations
+ * 
+ * @author Ally Rogers -- rogersal@grinnell.edu
+ */
 public class Utils {
+  /**
+   * Determine whether arr contains an element el where abs(el - d) < eps
+   * 
+   * @pre arr must be full and arr.length > 0
+   * @post Returns true if there is a element arr[i] in arr
+   *       such that abs(arr[i] - d) < eps, else returns false
+   */
   public static boolean contains(double[] arr, double eps, double d) {
     boolean contains = false;
     for (int i = 0; i < arr.length && !contains; i++) {
-      if ((arr[i] - d) < eps) {
+      if (Math.abs(arr[i] - d) < eps) {
         contains = true;
       } // if
     } // while
     return contains;
   } // contains(double[] arr, double eps, double d1)
   
-  // THIS IS WRONG -- LOOK AGAIN AT FORMULA (IT'S CHANGED)
+  /**
+   * Calculate x^y mod m
+   * 
+   * @pre None.
+   * @post Returns the result of x^y mod m
+   */
   public static int fastModExpt(int x, int y, int m) {
     int result;
-    if (y == 0 || y % 2 == 0) {
-      result = expt(((x * x) % m), y/2);
+    if (y == 0) {
+      return 1;
+    } // if
+    if (y == 1) {
+      return x;
+    } // if (base case)
+    if (y % 2 == 0) {
+      result = fastModExpt(((x * x) % m), y/2, m) % m;
     } else {
-      result = x * (expt(x, y-1) % m);
+      result = (x * fastModExpt(x, y-1, m)) % m;
     } // else
     return result;
   } // fastModExpt(int x, int y, int m)
   
-  public static int expt(int x, int y) {
-    if (y == 0) {
-      return 1;
-    } // if y is 0
-    int result = x;
-    for (int i = 0; i < y - 1; i++) {
-      result *= x;
-    } // for
-    return result;
-  } // expt(int x, int y)
-  
+  /**
+   * Finds all possible pairs of the elements of arr
+   * 
+   * @pre arr is full and arr.length > 0
+   * @post pairArr is an array of all possible pairs (including duplicate and reversed pairs)
+   *       that can be created using the elements of arr.
+   * @post pairArr.length = arr.length^2
+   */
   public static IntPair[] allPair(int[] arr) {
     IntPair[] pairArr = new IntPair[arr.length * arr.length];
     for (int i = 0; i < arr.length; i++) {
       for (int k = 0; k < arr.length; k++) {
-        pairArr[i*arr.length+k] = new IntPair(arr[i], arr[k]);
+        pairArr[i * arr.length + k] = new IntPair(arr[i], arr[k]);
       } // for
     } // for
     return pairArr;
   } // allPair(int[] arr)
   
+  /**
+   * g
+   * 
+   * @pre
+   * @post
+   */
   public static String concatAndReplicateAll(String[] arr, int n) {
     String result = new String("");
     for (int i = 0; i < arr.length; i++) {
@@ -53,38 +81,38 @@ public class Utils {
     return result;
   } // concatAndReplicateAll(String[] arr, int n)
   
+  /**
+   * g
+   * 
+   * @pre
+   * @post
+   */
   public static int[] interleave(int[] arr1, int[] arr2) {
     int[] result = new int[arr1.length + arr2.length];
+    int counter = 0;
     if (arr1.length > arr2.length) {
       for (int i = 0; i < arr2.length; i++) {
         result[i*2] = arr1[i];
         result[i*2 + 1] = arr2[i];
+        counter ++;
       } // for
       for (int i = arr2.length; i < arr1.length; i++) {
         result[arr2.length + i] = arr1[i];
+        counter++;
       } // for
     } else {
       for (int i = 0; i < arr1.length; i++) {
         result[i*2] = arr1[i];
         result[i*2 + 1] = arr2[i];
+        counter ++;
       } // for
       for (int i = arr1.length; i < arr2.length; i++) {
         result[arr1.length + i] = arr2[i];
+        counter++;
       } // for
     } // else
+    System.out.println("counter: " + counter + " for arr1.len: " + arr1.length + ", arr2.len: " + arr2.length);
     return result;
   } // interleave(int[] arr1, int[] arr2)
   
 } // class Utils
-
-/*
- * Questions:
- *  - IntPair I was supposed to make or to import from somewhere?
- *  - Do I need to avoid repeat pairs?
- *  - Is 0 a valid argument for fastModExpt?
- *  - NO, LOOK AGAIN - Is contains correct? The problem formatting is scrambled so I'm not sure I got the formula correct.
- *  - YES - Is it okay that I have all of my methods for all questions in one class called Utils?
- *  - YES - Are all the classes supposed to be static?
- *  - [You're Right] - What format should the time complexity analysis be in? Should we include counters in our original
- *    classes or make new duplicate classes for them?
- */
